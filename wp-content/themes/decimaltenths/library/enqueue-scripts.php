d5d5cbab67267b12e57f8a3dbfb32c71f65567c5
Caroline Hagan <caroline@blueocto.co.uk>
2022-07-07 13:18:16 +0100

    Not calling app.js so removed this line

--- wp-content/themes/decimaltenths/library/enqueue-scripts.php
+++ wp-content/themes/decimaltenths/library/enqueue-scripts.php
@@ -54,7 +54,7 @@ if ( ! function_exists( 'octopress_scripts' ) ) :
 
 			/* Enqueue our Theme scripts */
 
-			wp_enqueue_script( 'theme-app', get_template_directory_uri() . '/dist/js/' . octopress_asset_path( 'app.js' ), array(), '', false );
+			// wp_enqueue_script( 'theme-app', get_template_directory_uri() . '/dist/js/' . octopress_asset_path( 'app.js' ), array(), '', false );
 
 			wp_enqueue_script( 'theme-menu', get_stylesheet_directory_uri() . '/dist/vendor/' . octopress_asset_path( 'primary-navigation.js' ), '', '', false );
 
