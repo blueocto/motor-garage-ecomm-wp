d3288d8491e4252df059f2a9c91ed9ab0d1edb09
Caroline Hagan <caroline@blueocto.co.uk>
2022-07-13 13:17:42 +0100

    Moved sections to template-parts

--- wp-content/themes/decimaltenths/page-templates/homepage.php
+++ wp-content/themes/decimaltenths/page-templates/homepage.php
@@ -6,41 +6,9 @@ get_header(); ?>
 
 <main id="main" class="main">
 
-    <section class="hero-intro">
-        <div class="row">
-            <div class="column small-12 large-7 hero-intro--image">&nbsp;</div>
-            <div class="column small-12 large-5 hero-intro--content">
-                <h1><span class="small-h">Welcome to</span> Decimal Tenths</h1>
-                <p class="h6">Lorem ipsum dolor sit amet, consectetur adipiscing elit fuis  ultrices nunc. Cras id quam non odio egestas cursus nisi. Suspendisse potenti norbi a tristique.</p>
-                <p>Suspendisse iaculis auctor nunc a auctor. Duis molestie tellus enim, quis ultricies ante euismod a. Vestibulum arcu quam, rhoncus nec nisl vitae, dictum lobortis libero. Vestibulum ac risus leo. Curabitur ullamcorper volutpat tortor at dictum. In sagittis sit amet nisl convallis tempor. Mauris consectetur eget urna vitae egestas. Aliquam a augue sodales, mattis lacus gravida, lacinia ante.</p>
-                <p>Aliquam bibendum, quam sed sagittis vulputate, risus neque commodo ante, id tempor dolor nulla eu eros. Cras tortor sem, molestie vitae enim vel, scelerisque auctor nulla.</p>
-                <p>
-                    <a class="btn" href="#">Learn More</a>
-                </p>
-            </div>
-        </div>
-    </section>
+    <?php //get_template_part( 'template-parts/hero-intro' ); ?>
 
-    <section class="search-by-car-form">
-        <h5 class="sbcf--text">Search by car</h5>
-        <form class="sbcf--form">
-            <select>
-                <option value="">--Choose Your Manufacturer--</option>
-                <option value="">Audi</option>
-                <option value="">SEAT</option>
-                <option value="">Skoda</option>
-                <option value="">Volkswagen</option>
-            </select>
-            <select>
-                <option value="">--Model--</option>
-                <option value="">Amarok</option>
-                <option value="">Arteon</option>
-                <option value="">Golf</option>
-                <option value="">Yeti</option>
-            </select>
-            <input type="submit" value="Search" class="ico-search">
-        </form>
-    </section>
+    <?php //get_template_part( 'template-parts/search-by-car' ); ?>
 
     <section>
         <h2><span>Shop our</span> Latest Products</h2>
@@ -50,7 +18,7 @@ get_header(); ?>
                     <p>{img}</p>
                     <h3>ARP Crankshaft Bolt</h3>
                     <p>&pound;60.00</p>
-                    <p><a class="btn" href="#">Add To Cart</p>
+                    <p><a class="btn" href="#">Add To Cart</a></p>
                 </div>
             </div>
             <div class="column small-12 large-3">
@@ -58,7 +26,7 @@ get_header(); ?>
                     <p>{img}</p>
                     <h3>ARP Crankshaft Bolt</h3>
                     <p>&pound;60.00</p>
-                    <p><a class="btn" href="#">Add To Cart</p>
+                    <p><a class="btn" href="#">Add To Cart</a></p>
                 </div>
             </div>
             <div class="column small-12 large-3">
@@ -66,7 +34,7 @@ get_header(); ?>
                     <p>{img}</p>
                     <h3>ARP Crankshaft Bolt</h3>
                     <p>&pound;60.00</p>
-                    <p><a class="btn" href="#">Add To Cart</p>
+                    <p><a class="btn" href="#">Add To Cart</a></p>
                 </div>
             </div>
             <div class="column small-12 large-3">
@@ -74,7 +42,7 @@ get_header(); ?>
                     <p>{img}</p>
                     <h3>ARP Crankshaft Bolt</h3>
                     <p>&pound;60.00</p>
-                    <p><a class="btn" href="#">Add To Cart</p>
+                    <p><a class="btn" href="#">Add To Cart</a></p>
                 </div>
             </div>
         </div>
---
+++ wp-content/themes/decimaltenths/template-parts/search-by-car.php
@@ -0,0 +1,20 @@
+<section class="search-by-car-form">
+    <h5 class="sbcf--text">Search by car</h5>
+    <form class="sbcf--form">
+        <select>
+            <option value="">--Choose Your Manufacturer--</option>
+            <option value="">Audi</option>
+            <option value="">SEAT</option>
+            <option value="">Skoda</option>
+            <option value="">Volkswagen</option>
+        </select>
+        <select>
+            <option value="">--Model--</option>
+            <option value="">Amarok</option>
+            <option value="">Arteon</option>
+            <option value="">Golf</option>
+            <option value="">Yeti</option>
+        </select>
+        <input type="submit" value="Search" class="ico-search">
+    </form>
+</section>
