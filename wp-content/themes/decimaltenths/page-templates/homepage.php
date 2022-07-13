0ac4a4e431893f1cf30d90a698fdd91328abe004
Caroline Hagan <caroline@blueocto.co.uk>
2022-07-13 11:45:39 +0100

    HTML section for the Homepage Introduduction w/ styles. Button component added.

--- wp-content/themes/decimaltenths/page-templates/homepage.php
+++ wp-content/themes/decimaltenths/page-templates/homepage.php
@@ -6,14 +6,12 @@ get_header(); ?>
 
 <main id="main" class="main">
 
-    <section>
+    <section class="hero-intro">
         <div class="row">
-            <div class="column small-12 large-6">
-                <p>{img}</p>
-            </div>
-            <div class="column small-12 large-6">
-                <h1><span>Welcome to</span> Decimal Tenths</h1>
-                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit fuis  ultrices nunc. Cras id quam non odio egestas cursus nisi. Suspendisse potenti norbi a tristique.</h2>
+            <div class="column small-12 large-7 hero-intro--image">&nbsp;</div>
+            <div class="column small-12 large-5 hero-intro--content">
+                <h1><span class="small-h">Welcome to</span> Decimal Tenths</h1>
+                <p class="h6">Lorem ipsum dolor sit amet, consectetur adipiscing elit fuis  ultrices nunc. Cras id quam non odio egestas cursus nisi. Suspendisse potenti norbi a tristique.</p>
                 <p>Suspendisse iaculis auctor nunc a auctor. Duis molestie tellus enim, quis ultricies ante euismod a. Vestibulum arcu quam, rhoncus nec nisl vitae, dictum lobortis libero. Vestibulum ac risus leo. Curabitur ullamcorper volutpat tortor at dictum. In sagittis sit amet nisl convallis tempor. Mauris consectetur eget urna vitae egestas. Aliquam a augue sodales, mattis lacus gravida, lacinia ante.</p>
                 <p>Aliquam bibendum, quam sed sagittis vulputate, risus neque commodo ante, id tempor dolor nulla eu eros. Cras tortor sem, molestie vitae enim vel, scelerisque auctor nulla.</p>
                 <p>
