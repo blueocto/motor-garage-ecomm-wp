371ca22394133de952e724e19f3abeef5c1c8bc4
Caroline Hagan <caroline@blueocto.co.uk>
2022-07-20 21:28:21 +0100

    JS wrapper to control layout on Product

--- wp-content/themes/decimaltenths/src/js/app.js
+++ wp-content/themes/decimaltenths/src/js/app.js
@@ -4,4 +4,7 @@ jQuery(document).ready(function ($) {
     // wrap a container around the Title and Description on a Taxonomy/shop category page
     $( ".woocommerce-products-header .page-title, .woocommerce-products-header .term-description" ).wrapAll( "<div class='taxonomy-head-wrap'/>" );
 
+    // wrap a container around the single product entry-summary and tabs
+    $(".entry-summary, .woocommerce-tabs").wrapAll('<div class="single-product-description"></div>');
+
 });
