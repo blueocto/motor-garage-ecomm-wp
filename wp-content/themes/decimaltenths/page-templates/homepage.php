9f5463a01dd7ba2002bb4a7c4a69e22892fd79a6
Caroline Hagan <caroline@blueocto.co.uk>
2022-09-28 13:32:50 +0100

    Initial homepage template layout. Added dev guides

--- wp-content/themes/decimaltenths/page-templates/homepage.php
+++ wp-content/themes/decimaltenths/page-templates/homepage.php
@@ -6,190 +6,62 @@ get_header(); ?>
 
 <main id="main" class="main">
 
+    <hero-car-filters>
+        <h2>Quality Parts</h2>
+        <p>For your beloved Audi, SEAT, VW and more&hellip;</p>
+        <?php get_template_part( 'template-parts/search-by-car' ); ?>
+    </hero-car-filters>
 
-    <?php get_template_part( 'template-parts/search-by-car' ); ?>
+    <brand-logos>
+        <p>[ Affiliate logo's ]</p>
+    </brand-logos>
 
-    <?php /* ?>
-    <section>
-        <h2><span>Shop our</span> Latest Products</h2>
-        <div class="row">
-            <div class="column small-12 large-3">
-                <div class="product">
-                    <p>{img}</p>
-                    <h3>ARP Crankshaft Bolt</h3>
-                    <p>&pound;60.00</p>
-                    <p><a class="btn" href="#">Add To Cart</a></p>
-                </div>
-            </div>
-            <div class="column small-12 large-3">
-                <div class="product">
-                    <p>{img}</p>
-                    <h3>ARP Crankshaft Bolt</h3>
-                    <p>&pound;60.00</p>
-                    <p><a class="btn" href="#">Add To Cart</a></p>
-                </div>
-            </div>
-            <div class="column small-12 large-3">
-                <div class="product">
-                    <p>{img}</p>
-                    <h3>ARP Crankshaft Bolt</h3>
-                    <p>&pound;60.00</p>
-                    <p><a class="btn" href="#">Add To Cart</a></p>
-                </div>
-            </div>
-            <div class="column small-12 large-3">
-                <div class="product">
-                    <p>{img}</p>
-                    <h3>ARP Crankshaft Bolt</h3>
-                    <p>&pound;60.00</p>
-                    <p><a class="btn" href="#">Add To Cart</a></p>
-                </div>
-            </div>
-        </div>
-    </section>
-    <?php */ ?>
+    <scrolling-products>
+        <h2>New In</h2>
+        <div>[ scrolling products - new feed ]</div>
+    </scrolling-products>
 
-    <?php /* ?>
-    <section>
-        <div class="row">
-            <div class="column large-fifth">
-                <p>{ico}</p>
-                <h3>Servicing</h3>
-                <p>Lorem ipsum dolor sit amet porti elit consectetur adipisc elit imi surabitur vestibulum rhindus.</p>
-                <p><a href="#">Discover</a></p>
-            </div>
-            <div class="column large-fifth">
-                <p>{ico}</p>
-                <h3>Servicing</h3>
-                <p>Lorem ipsum dolor sit amet porti elit consectetur adipisc elit imi surabitur vestibulum rhindus.</p>
-                <p><a href="#">Discover</a></p>
-            </div>
-            <div class="column small-12 large-fifth">
-                <p>{ico}</p>
-                <h3>Servicing</h3>
-                <p>Lorem ipsum dolor sit amet porti elit consectetur adipisc elit imi surabitur vestibulum rhindus.</p>
-                <p><a href="#">Discover</a></p>
-            </div>
-            <div class="column small-12 large-fifth">
-                <p>{ico}</p>
-                <h3>Servicing</h3>
-                <p>Lorem ipsum dolor sit amet porti elit consectetur adipisc elit imi surabitur vestibulum rhindus.</p>
-                <p><a href="#">Discover</a></p>
-            </div>
-            <div class="column small-12 large-fifth">
-                <p>{ico}</p>
-                <h3>Servicing</h3>
-                <p>Lorem ipsum dolor sit amet porti elit consectetur adipisc elit imi surabitur vestibulum rhindus.</p>
-                <p><a href="#">Discover</a></p>
-            </div>
-        </div>
-    </section>
-    <?php */ ?>
+    <category-panels>
+        <h2>Browse our categories</h2>
+        <panel>
+            <h3>Engines</h3>
+            <p><a href="#">Browse</a></p>
+        </panel>
+        <panel>
+            <h3>Lorum</h3>
+            <p><a href="#">Browse</a></p>
+        </panel>
+        <panel>
+            <h3>Ipsum</h3>
+            <p><a href="#">Browse</a></p>
+        </panel>
+        <panel>
+            <h3>Hover</h3>
+            <p><a href="#">Browse</a></p>
+        </panel>
+        <panel>
+            <h3>Amet</h3>
+            <p><a href="#">Browse</a></p>
+        </panel>
+    </category-panels>
 
-    <?php /* ?>
-    <section class="row">
-        <div class="column small-12">
-            <h2><span>Engine Services</span> Custom Tuning</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisc elit. Curabitur vestibulum purus sed porta biben dumresi donec rhoncus enim nisl.</p>
-            <p><a class="btn btn-outline" href="#">Discover</a></p>
-        </div>
-    </section>
-    <?php */ ?>
+    <scrolling-products>
+        <h2>Popular</h2>
+        <div>[ scrolling products - popular ]</div>
+    </scrolling-products>
 
-    <?php /* ?>
-    <section>
-        <h2><span>Check out our</span> YouTube Channel&hellip;</h2>
-        <div class="row">
-            <div class="column small-12 large-2">
-                <p>{video clip}</p>
-                <h3>VIS Motorsport Balance Sh..</h3>
-                <p>11/02/2022</p>
-                <p>In this video I tackle the MK5 GTI track car engine, with a Vis...</p>
-                <p><strong>3.8K Views &bull; 216 Likes</strong></p>
-            </div>
-            <div class="column small-12 large-2">
-                <p>{video clip}</p>
-                <h3>VIS Motorsport Balance Sh..</h3>
-                <p>11/02/2022</p>
-                <p>In this video I tackle the MK5 GTI track car engine, with a Vis...</p>
-                <p><strong>3.8K Views &bull; 216 Likes</strong></p>
-            </div>
-            <div class="column small-12 large-2">
-                <p>{video clip}</p>
-                <h3>VIS Motorsport Balance Sh..</h3>
-                <p>11/02/2022</p>
-                <p>In this video I tackle the MK5 GTI track car engine, with a Vis...</p>
-                <p><strong>3.8K Views &bull; 216 Likes</strong></p>
-            </div>
-            <div class="column small-12 large-2">
-                <p>{video clip}</p>
-                <h3>VIS Motorsport Balance Sh..</h3>
-                <p>11/02/2022</p>
-                <p>In this video I tackle the MK5 GTI track car engine, with a Vis...</p>
-                <p><strong>3.8K Views &bull; 216 Likes</strong></p>
-            </div>
-            <div class="column small-12 large-2">
-                <p>{video clip}</p>
-                <h3>VIS Motorsport Balance Sh..</h3>
-                <p>11/02/2022</p>
-                <p>In this video I tackle the MK5 GTI track car engine, with a Vis...</p>
-                <p><strong>3.8K Views &bull; 216 Likes</strong></p>
-            </div>
-            <div class="column small-12 large-2">
-                <p>{video clip}</p>
-                <h3>VIS Motorsport Balance Sh..</h3>
-                <p>11/02/2022</p>
-                <p>In this video I tackle the MK5 GTI track car engine, with a Vis...</p>
-                <p><strong>3.8K Views &bull; 216 Likes</strong></p>
-            </div>
-        </div>
-        <hr />
-        <div class="row">
-            <div class="column small-12 large-expand">
-                <p>{ico}</p>
-                <h4>Decimal Tenths</h4>
-                <p><strong>8.1k Subscribers &bull; 112 Videos &bull; 642k Views</strong></p>
-                <p><em>Track Cars, Engine Builds, Witty Banter&hellip; OK mybe not the last one&hellip;</p>
-            </div>
-            <div class="column small-12">
-                <p><a class="btn btn-outline" href="#">Visit our channel!</a></p>
-            </div>
-        </div>
-    </section>
-    <?php */ ?>
+    <service-highlight>
+        <h2>Custom Tuning</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo pulvinar lacus, et porta diam rutrum non.</p>
+        <p><a href="#">View services</a></p>
+    </service-highlight>
 
-    <?php /* ?>
-    <section class="row">
-        <div class="column small-12">
-            <h2>Testimonials</h2>
-            <h3>What our customers say&hellip;</h3>
-            <p>Suspendisse iaculis auctor nunc a auctor. Duis molestie tellus enim, quis ultricies ante euismod a. Vestibulum arcu quam, rhoncus nec nisl vitae, dictum lobortis libero. Vestibulum ac risus leo. Curabitur ullamcorper volutpat tortor at dictum. In sagittis sit amet nisl convallis tempor. Mauris consectetur eget urna vitae egestas. Aliquam a augue sodales, mattis lacus gravida, lacinia ante.</p>
-            <p>Aliquam bibendum, quam sed sagittis vulputate, risus neque commodo ante, id tempor dolor nulla eu eros. Cras tortor sem, molestie vitae enim vel, scelerisque auctor nulla.</p>
-            <p><strong><em>Jake Smith, Durham</em></strong></p>
-        </div>
-    </section>
-    <?php */ ?>
+    <video-feed>
+        <h2>Latest Videos <a href="#">View All</a></h2>
+        <p>[ Youtube Channel Feed ]</p>
+    </video-feed>
 
-    <?php /* ?>
-    <section>
-        <div class="row">
-            <div class="column small-12 large-6">
-                <h2><span>Contact</span><h2>
-                <h3>Get in touch with us</h3>
-                <p>Based in Blaydon On Tyne, Serving the whole of the North East, Northumberland, Newcastle, Gateshead, Durham, Teeside, Tyne And Wear and in fact…..the rest of the UK.</p>
-                <p>Great Products, Great Prices, Great Banter…. Aye…. First two anyway.</p>
-                <p>Fancy a trip out?… Low on Coffee?… Visit us at:</p>
-                <p><strong>Decimal Tenths, Unit 8, Imperial Business Park, Factory Road, Blaydon On Tyne, NE21 5SA</strong></p>
-                <p><a class="btn" href="#">Send a message</a></p>
-            </div>
-            <div class="column small-12 large-6">
-                <p>{img}</p>
-            </div>
-        </div>
-    </section>
-    <?php */ ?>
-
-	<?php if ( have_posts() ) : ?>
+	<?php /*if ( have_posts() ) : ?>
 	
 		<?php while ( have_posts() ) : the_post(); ?>	
 			<?php get_template_part( 'template-parts/content-page' ); ?>
@@ -199,7 +71,7 @@ get_header(); ?>
 			
 		<?php get_template_part( 'template-parts/content-none' ); ?>
 
-	<?php endif; // End have_posts() check. ?>
+	<?php endif; // End have_posts() check. */ ?>
 </main>
 
 <?php get_footer();
