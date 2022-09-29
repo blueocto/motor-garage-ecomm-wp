341fab2c83f3d0b5d6ff984c24cfa1fc8ede1f12
Caroline Hagan <caroline@blueocto.co.uk>
2022-09-29 12:25:19 +0100

    More Footer styles

--- wp-content/themes/decimaltenths/footer.php
+++ wp-content/themes/decimaltenths/footer.php
@@ -1,6 +1,6 @@
 	<footer class="footer" role="contentinfo">
 
-		<div class="row expanded">
+		<div class="row">
 			<div class="column">
 				<p class="logo">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" title="<?php bloginfo( 'name' ); ?>">
@@ -13,12 +13,12 @@
 				<h3>Shop</h3>
 				<nav>
 					<ul class="menu footer--menu">
-						<li><a href="#">Shop All</a></li>
-						<li><a href="#">Engines</a></li>
-						<li><a href="#">ECU</a></li>
-						<li><a href="#">Exhausts</a></li>
-						<li><a href="#">Suspension</a></li>
-						<li><a href="#">Merch</a></li>
+						<li class="menu-item"><a href="#">Shop All</a></li>
+						<li class="menu-item"><a href="#">Engines</a></li>
+						<li class="menu-item"><a href="#">ECU</a></li>
+						<li class="menu-item"><a href="#">Exhausts</a></li>
+						<li class="menu-item"><a href="#">Suspension</a></li>
+						<li class="menu-item"><a href="#">Merch</a></li>
 					</ul>
 				</nav>
 			</div>
@@ -26,11 +26,11 @@
 				<h3>Customer</h3>
 				<nav>
 					<ul class="menu footer--menu">
-						<li><a href="#">Account</a></li>
-						<li><a href="#">Delivery</a></li>
-						<li><a href="#">Returns</a></li>
-						<li><a href="#">FAQ</a></li>
-						<li><a href="#">Terms</a></li>
+						<li class="menu-item"><a href="#">Account</a></li>
+						<li class="menu-item"><a href="#">Delivery</a></li>
+						<li class="menu-item"><a href="#">Returns</a></li>
+						<li class="menu-item"><a href="#">FAQ</a></li>
+						<li class="menu-item"><a href="#">Terms</a></li>
 					</ul>
 				</nav>
 			</div>
@@ -38,8 +38,8 @@
 				<h3>Services</h3>
 				<nav>
 					<ul class="menu footer--menu">
-						<li><a href="#">All Services</a></li>
-						<li><a href="#">Custom Tuning</a></li>
+						<li class="menu-item"><a href="#">All Services</a></li>
+						<li class="menu-item"><a href="#">Custom Tuning</a></li>
 					</ul>
 				</nav>
 			</div>
@@ -47,20 +47,21 @@
 				<h3>Social</h3>
 				<nav>
 					<ul class="menu footer--menu">
-						<li><a href="#">YouTube</a></li>
-						<li><a href="#">Twitter</a></li>
-						<li><a href="#">Facebook</a></li>
-						<li><a href="#">Instagram</a></li>
-						<li><a href="#">LinkedIn</a></li>
+						<li class="menu-item"><a href="#">YouTube</a></li>
+						<li class="menu-item"><a href="#">Twitter</a></li>
+						<li class="menu-item"><a href="#">Facebook</a></li>
+						<li class="menu-item"><a href="#">Instagram</a></li>
+						<li class="menu-item"><a href="#">LinkedIn</a></li>
 					</ul>
 				</nav>
 			</div>
 		</div>
 		<footer-summary>
 			<p>Decimal Tenths offers performance parts, tuning services, laser alignment, all under one roof. Great prices on Tarox, Cobra, Powerflex, Forge Motorsport and so much more. Why not call past Blaydon on Tyne and see the Furious Frog in in real life, or feel free to browse online our wide range of parts and accessories available to collect from HQ or delivered to the North East, Newcastle upon Tyne, Gateshead, Durham, Northumberland, Teesside, Tyne and Wear and the rest of the UK.</p>
+
+			<p class="copyright">&copy; Copyright <?php print date("Y"); ?> Decimal Tenths. Company No. 12818455 | All Rights Reserved</p>
+			<p class="credit"><a href="https://blueocto.co.uk" target="_blank" rel="noopener noreferrer"><span class="visuallyhidden">WordPress WooCommerce Website by </span>Blueocto Ltd</a></p>
 		</footer-summary>
-		<p class="copyright">&copy; Copyright <?php print date("Y"); ?> Decimal Tenths. Company No. 12818455 | All Rights Reserved</p>
-		<p class="credit"><a href="https://blueocto.co.uk" target="_blank" rel="noopener nofollow"><span class="visuallyhidden">WordPress WooCommerce Website by </span>Blueocto Ltd</a></p>
 	</footer>
 
 </div><!-- // bo-content -->
