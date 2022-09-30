4a25ccbe3c5a1e8f7bf0b126a3c22afd4deb3450
Caroline Hagan <caroline@blueocto.co.uk>
2022-09-30 14:31:33 +0100

    Moved row to the parent element

--- wp-content/themes/decimaltenths/page-templates/homepage.php
+++ wp-content/themes/decimaltenths/page-templates/homepage.php
@@ -16,19 +16,17 @@ get_header(); ?>
     </hero-car-filters>
 
     <brand-logos>
-        <div class="row">
-            <div class="column">
-                <img class="b-logo" src="<?php echo get_stylesheet_directory_uri() . '/dist/svg/audi-white.svg'; ?>" alt="" />
-            </div>
-            <div class="column">
-                <img class="b-logo" src="<?php echo get_stylesheet_directory_uri() . '/dist/svg/seat-white.svg'; ?>" alt="" />
-            </div>
-            <div class="column">
-                <img class="b-logo" src="<?php echo get_stylesheet_directory_uri() . '/dist/svg/skoda-white.svg'; ?>" alt="" />
-            </div>
-            <div class="column">
-                <img class="b-logo" src="<?php echo get_stylesheet_directory_uri() . '/dist/svg/volkswagen-white.svg'; ?>" alt="" />
-            </div>
+        <div class="column">
+            <img class="b-logo" src="<?php echo get_stylesheet_directory_uri() . '/dist/svg/audi-white.svg'; ?>" alt="" />
+        </div>
+        <div class="column">
+            <img class="b-logo" src="<?php echo get_stylesheet_directory_uri() . '/dist/svg/seat-white.svg'; ?>" alt="" />
+        </div>
+        <div class="column">
+            <img class="b-logo" src="<?php echo get_stylesheet_directory_uri() . '/dist/svg/skoda-white.svg'; ?>" alt="" />
+        </div>
+        <div class="column">
+            <img class="b-logo" src="<?php echo get_stylesheet_directory_uri() . '/dist/svg/volkswagen-white.svg'; ?>" alt="" />
         </div>
     </brand-logos>
 
