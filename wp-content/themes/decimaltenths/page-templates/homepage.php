b88345c574943fb6572f78cd8c27f0274878f6df
Caroline Hagan <caroline@blueocto.co.uk>
2022-10-09 17:53:36 +0100

    Moved "service-highlight" section to a part

--- wp-content/themes/decimaltenths/page-templates/homepage.php
+++ wp-content/themes/decimaltenths/page-templates/homepage.php
@@ -23,31 +23,7 @@ get_header(); ?>
 
     <?php get_template_part( 'template-parts/blocks/popular-products' ); ?>
 
-    <service-highlight>
-        <div class="picture highlight-img-1">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0232.webp'; ?>" alt="" />
-        </div>
-        <div class="picture highlight-img-2">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0100.webp'; ?>" alt="" />
-        </div>
-        <div class="picture highlight-img-3">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0222.webp'; ?>" alt="" />
-        </div>
-        <div class="highlight-content">
-            <h2>Custom Tuning</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo pulvinar lacus, et porta diam rutrum non.</p>
-            <p><a class="btn" href="#">View services</a></p>
-        </div>
-        <div class="picture highlight-img-4">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0106.webp'; ?>" alt="" />
-        </div>
-        <div class="picture highlight-img-5">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0150.webp'; ?>" alt="" />
-        </div>
-        <div class="picture highlight-img-6">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0166.webp'; ?>" alt="" />
-        </div>
-    </service-highlight>
+    <?php get_template_part( 'template-parts/blocks/service-highlight' ); ?>
 
     <video-feed>
         <div class="row column">
