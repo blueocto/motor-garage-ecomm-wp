fdd9e0ae44d878b9fb557ff7bb95accfda3feaab
Caroline Hagan <caroline@blueocto.co.uk>
2022-10-09 16:36:12 +0100

    Responsive styles for the "category panels" section

--- wp-content/themes/decimaltenths/page-templates/homepage.php
+++ wp-content/themes/decimaltenths/page-templates/homepage.php
@@ -34,26 +34,34 @@ get_header(); ?>
 
     <category-panels>
         <h2 class="visuallyhidden">Browse our categories</h2>
-        <div class="row column">
-            <panel>
+        <div class="row">
+            <panel class="column">
                 <h3 class="h4">Engines</h3>
-                <p><a href="#">Browse</a></p>
+                <p><a class="arrow-right" href="#">Browse</a></p>
+                <picture class="picture">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+                </picture>
             </panel>
-            <panel>
+            <panel class="column">
                 <h3 class="h4">Lorum</h3>
-                <p><a href="#">Browse</a></p>
+                <p><a class="arrow-right" href="#">Browse</a></p>
+                <picture class="picture">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+                </picture>
             </panel>
-            <panel>
+            <panel class="column">
                 <h3 class="h4">Ipsum</h3>
-                <p><a href="#">Browse</a></p>
+                <p><a class="arrow-right" href="#">Browse</a></p>
+                <picture class="picture">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+                </picture>
             </panel>
-            <panel>
+            <panel class="column">
                 <h3 class="h4">Hover</h3>
-                <p><a href="#">Browse</a></p>
-            </panel>
-            <panel>
-                <h3 class="h4">Amet</h3>
-                <p><a href="#">Browse</a></p>
+                <p><a class="arrow-right" href="#">Browse</a></p>
+                <picture class="picture">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+                </picture>
             </panel>
         </div>
     </category-panels>
