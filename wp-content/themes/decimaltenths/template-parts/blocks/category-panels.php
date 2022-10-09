ef79a8d2fa781ca113e9fbed61ac55491e7056ce
Caroline Hagan <caroline@blueocto.co.uk>
2022-10-09 17:45:15 +0100

    Restructured the "category panels" to keep semantics but achieve hover states

--- wp-content/themes/decimaltenths/template-parts/blocks/category-panels.php
+++ wp-content/themes/decimaltenths/template-parts/blocks/category-panels.php
@@ -3,31 +3,47 @@
     <div class="row">
         <panel class="column">
             <h3 class="h4">Engines</h3>
-            <p><a class="arrow-right" href="#">Browse</a></p>
-            <picture class="picture">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
-            </picture>
+            <p>
+                <a href="#">
+                    <span class="arrow-right">Browse</span>
+                    <span class="picture">
+                        <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0193.webp'; ?>" alt="" />
+                    </span>
+                </a>
+            </p>
         </panel>
         <panel class="column">
             <h3 class="h4">Lorum</h3>
-            <p><a class="arrow-right" href="#">Browse</a></p>
-            <picture class="picture">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
-            </picture>
+            <p>
+                <a href="#">
+                    <span class="arrow-right">Browse</span>
+                    <span class="picture">
+                        <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0200.webp'; ?>" alt="" />
+                    </span>
+                </a>
+            </p>
         </panel>
         <panel class="column">
             <h3 class="h4">Ipsum</h3>
-            <p><a class="arrow-right" href="#">Browse</a></p>
-            <picture class="picture">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
-            </picture>
+            <p>
+                <a href="#">
+                    <span class="arrow-right">Browse</span>
+                    <span class="picture">
+                        <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0194.webp'; ?>" alt="" />
+                    </span>
+                </a>
+            </p>
         </panel>
         <panel class="column">
             <h3 class="h4">Hover</h3>
-            <p><a class="arrow-right" href="#">Browse</a></p>
-            <picture class="picture">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
-            </picture>
+            <p>
+                <a href="#">
+                    <span class="arrow-right">Browse</span>
+                    <span class="picture">
+                        <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0221.webp'; ?>" alt="" />
+                    </span>
+                </a>
+            </p>
         </panel>
     </div>
 </category-panels>
