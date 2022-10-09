dbd0c2fd30f522b7d6e835040bdb7820206bb4d5
Caroline Hagan <caroline@blueocto.co.uk>
2022-10-09 17:52:23 +0100

    Desktop styles for the "custom tuning/service-highlight" section

--- wp-content/themes/decimaltenths/page-templates/homepage.php
+++ wp-content/themes/decimaltenths/page-templates/homepage.php
@@ -25,13 +25,13 @@ get_header(); ?>
 
     <service-highlight>
         <div class="picture highlight-img-1">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0232.webp'; ?>" alt="" />
         </div>
         <div class="picture highlight-img-2">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0100.webp'; ?>" alt="" />
         </div>
         <div class="picture highlight-img-3">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0222.webp'; ?>" alt="" />
         </div>
         <div class="highlight-content">
             <h2>Custom Tuning</h2>
@@ -39,13 +39,13 @@ get_header(); ?>
             <p><a class="btn" href="#">View services</a></p>
         </div>
         <div class="picture highlight-img-4">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0106.webp'; ?>" alt="" />
         </div>
         <div class="picture highlight-img-5">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0150.webp'; ?>" alt="" />
         </div>
         <div class="picture highlight-img-6">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/green-car-bg.jpg'; ?>" alt="" />
+            <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0166.webp'; ?>" alt="" />
         </div>
     </service-highlight>
 
