487b2420f3de7a9a8b6bc07bd279606f2c2974c6
Daniel Dixon <daniel@blueocto.co.uk>
2022-11-01 14:47:59 +0000

    Category panels now managed by ACF fields (theme options)

--- wp-content/themes/decimaltenths/template-parts/blocks/category-panels.php
+++ wp-content/themes/decimaltenths/template-parts/blocks/category-panels.php
@@ -1,49 +1,28 @@
 <category-panels>
     <h2 class="visuallyhidden">Browse our categories</h2>
     <div class="row">
-        <panel class="column">
-            <h3 class="h4">Engines</h3>
-            <p>
-                <a href="#">
-                    <span class="arrow-right">Browse</span>
-                    <span class="picture">
-                        <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0193.webp'; ?>" alt="" />
-                    </span>
-                </a>
-            </p>
-        </panel>
-        <panel class="column">
-            <h3 class="h4">Lorum</h3>
-            <p>
-                <a href="#">
-                    <span class="arrow-right">Browse</span>
-                    <span class="picture">
-                        <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0200.webp'; ?>" alt="" />
-                    </span>
-                </a>
-            </p>
-        </panel>
-        <panel class="column">
-            <h3 class="h4">Ipsum</h3>
-            <p>
-                <a href="#">
-                    <span class="arrow-right">Browse</span>
-                    <span class="picture">
-                        <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0194.webp'; ?>" alt="" />
-                    </span>
-                </a>
-            </p>
-        </panel>
-        <panel class="column">
-            <h3 class="h4">Hover</h3>
-            <p>
-                <a href="#">
-                    <span class="arrow-right">Browse</span>
-                    <span class="picture">
-                        <img src="<?php echo get_stylesheet_directory_uri() . '/dist/img/SBM0221.webp'; ?>" alt="" />
-                    </span>
-                </a>
-            </p>
-        </panel>
+        <?php 
+        if( have_rows('category_panel', 'option') ):
+            ?>
+            <?php
+            while ( have_rows( 'category_panel', 'option' ) ) : the_row();
+            ?>
+                <panel class="column">
+                    <h3 class="h4"><?php print(get_sub_field("name", 'option')); ?></h3>
+                    <p>
+                        <a href="<?php print(get_sub_field("link", 'option')); ?>">
+                            <span class="arrow-right">Browse</span>
+                            <span class="picture">
+                                <?php print(wp_get_attachment_image( get_sub_field("image", 'option'), "large", array( 'loading' => 'lazy'))); ?>
+                            </span>
+                        </a>
+                    </p>
+                </panel>
+            <?php
+            endwhile;
+            ?>
+        <?php
+        endif;
+        ?>
     </div>
 </category-panels>
