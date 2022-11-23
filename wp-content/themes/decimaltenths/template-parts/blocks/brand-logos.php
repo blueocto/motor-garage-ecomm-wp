49abd2a136e23a7fda56ebf65e29ccfd5247f78e
Caroline Hagan <caroline@blueocto.co.uk>
2022-11-23 14:29:46 +0000

    Applied the background texture to brand logo's as these are white. Reduced height/whitespace

--- wp-content/themes/decimaltenths/template-parts/blocks/brand-logos.php
+++ wp-content/themes/decimaltenths/template-parts/blocks/brand-logos.php
@@ -1,19 +1,11 @@
 <brand-logos>
-    <?php 
-    if( have_rows('brand_logos', 'option') ):
-        ?>
-        <?php
-        while ( have_rows( 'brand_logos', 'option' ) ) : the_row();
-        ?>
-        <div class="column">
-            <?php
-            echo wp_get_attachment_image( get_sub_field('logo', 'option'), "large", "", array("class" => "b-logo") );
-            ?>
-        </div>
-        <?php
-        endwhile;
-        ?>
-    <?php
-    endif;
-    ?>
+    <div class="row">
+        <?php if( have_rows('brand_logos', 'option') ): ?>
+            <?php while ( have_rows( 'brand_logos', 'option' ) ) : the_row(); ?>
+            <div class="column">
+                <?php echo wp_get_attachment_image( get_sub_field('logo', 'option'), "120", "", array("class" => "b-logo") ); ?>
+            </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 </brand-logos>
